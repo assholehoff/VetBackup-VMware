8283ccaa2be7aab178f0533471842646dadea298
Anton Dahlén <39925638+assholehoff@users.noreply.github.com>
2026-08-27 13:36:01 +0200

    add new files to Windows project

---
+++ Windows/VetBackup/VetBackup.LastModifiedDate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+// VetBackup.LastModifiedDate - A simple utility to export the last modified date of the Vetvision database.
+// Anton Dahlén, 2026
+
+try {
+    string sourceFile = args.Length > 0 ? args[0] : @"C:\VetVision\VETDB\Database.fdb";
+    string outputFile = args.Length > 1 ? args[1] : @"V:\.DatabaseLastModifiedDate";
+
+    if (!File.Exists(sourceFile)) {
+        Console.WriteLine($"Error: Source file '{sourceFile}' does not exist.");
+        return 1;
+    }
+
+    DateTime modifiedDate = File.GetLastWriteTime(sourceFile);
+    string dateString = modifiedDate.ToString("yyyyMMdd-HHmmss");
+
+    File.WriteAllText(outputFile, dateString);
+    Console.WriteLine($"{sourceFile} last modified date: {dateString}");
+    return 0;
+} catch (Exception ex) {
+    Console.WriteLine($"Error: {ex.Message}");
+    return 1;
+}
