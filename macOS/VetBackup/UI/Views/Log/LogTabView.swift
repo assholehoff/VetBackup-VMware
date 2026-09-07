e106ee0e185f2f86888955a0591b63069a27c382
Anton Dahlén <39925638+assholehoff@users.noreply.github.com>
2026-09-07 14:50:07 +0200

    move files

---
+++ macOS/VetBackup/UI/Views/Log/LogTabView.swift
@@ -0,0 +1,17 @@
+//
+//  LogTab.swift
+//  VetBackup
+//
+//  Created by Anton Dahlén on 2026-09-07.
+//
+
+import SwiftUI
+
+struct LogTabView: View {
+    var log: Log
+    var body: some View {
+        List(log.book) { entry in
+            LogEntryView(entry: entry)
+        }
+    }
+}
